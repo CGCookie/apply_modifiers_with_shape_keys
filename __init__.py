66fa6d307428d467ee320eae87bd5ad8ccf3c30d
Wayne Dixon <thewaylow@gmail.com>
2025-10-03 16:29:04 +1000

    Added extra register line so it is visible in the F3 search

--- __init__.py
+++ __init__.py
@@ -26,21 +26,27 @@ def menu_func(self, context):
     self.layout.separator()  # Add a separator before the operator for a cleaner look
     self.layout.operator(OBJECT_OT_apply_modifiers_with_shape_keys.bl_idname)
 
+
 classes = [
     ModifierList,
     OBJECT_OT_apply_modifiers_with_shape_keys,
 ]
 
+
+def menu_func_global(self, context):
+    self.layout.operator(OBJECT_OT_apply_modifiers_with_shape_keys.bl_idname)
+
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     bpy.types.MESH_MT_shape_key_context_menu.append(menu_func)
+    # force it to show in the F3 menu
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
 
 def unregister():
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+    bpy.types.MESH_MT_shape_key_context_menu.remove(menu_func)
     for cls in classes:
         bpy.utils.unregister_class(cls)
-    bpy.types.MESH_MT_shape_key_context_menu.remove(menu_func)
-
-
-if __name__ == "__main__":
-    register()
